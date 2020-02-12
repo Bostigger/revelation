7e88737e0acfee75f8fc4a694345e5e43662b6b8
Clement Sam <clementsam75@gmail.com>
2020-02-12 12:04:06 +0000

    Fix dashboard

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -10,20 +10,19 @@ class NotifyController extends Controller
     //
     public function sendCodes()
     {
-        $kt_residents = KtResident::all(['name','code','contact_no'])->sortBy('name');
+        $kt_residents = KtResident::all(['id','name','code','contact_no'])->sortBy('name');
         //dd($kt_residents);
+        $sendNow = false;
+        /*
         foreach ($kt_residents as $kt_resident) {
-            if($kt_resident->contact_no) {
-                $message = 'KT Awards night comes off on Saturday, 7th March, 2020. You are entreated to join the award nomination exercise by using the access code ' . $kt_resident->code . ' to nominate members for the various categories.Thank you.';
-                $url = 'https://smartsmsgh.com/api/send';
-                $tokenUsername = 'contact';
-                $tokenPassword = 'adimcoam';
-                $phoneNumber = '233558899735';
-                $params = '?tokenUsername=' . $tokenUsername;
-                $params .= '&tokenPassword=' . $tokenPassword;
-                $params .= '&to=' . urlencode($phoneNumber);
-                $params .= '&from=' . urlencode('KTH-JCR');
-                $params .= '&content=' . urlencode($message);
+            if($kt_resident->contact_no && strlen($kt_resident->contact_no)===9 && $sendNow===true) {
+                $url = 'http://api.smsonlinegh.com/sendsms.php';
+                $params = '?user=clementsam75%40gmail.com';
+                $params .= '&password=clems%26norb1';
+                $params .= '&destination=' . urlencode('233'.$kt_resident->contact_no);
+                $params .= '&sender=' . urlencode('KTH-JCR');
+                $params .= '&type=0';
+                $params .= '&message=' . urlencode('KT Awards night comes off on Saturday, 7th March, 2020. You are entreated to join the award nomination exercise by using the access code ' . $kt_resident->code . ' to nominate members for the various categories. Visit http://bit.ly/umat-kth-awards to vote now. Thank you.');
                 // Send through Curl
                 $ch = curl_init();
                 $headers = array('Content-Type: multipart/form-data');
@@ -36,12 +35,13 @@ class NotifyController extends Controller
                 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
                 $result = curl_exec($ch);
                 $error = curl_error($ch);
-                echo "Message sent to " . $kt_resident->contact_no . ' ' . $result . '\n';
+                echo 'Message sent to ' . $kt_resident->name.' with respose: '.$result.'<br/>';
                 curl_close($ch);
-                break;
+            }
+            if ($kt_resident->id==322) {
+                $sendNow = true;
             }
         }
-        /*
         */
     }
 }
