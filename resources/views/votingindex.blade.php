752bcf44f907d245ed29aead08cbe1a1390100f5
Clement Sam <clementsam75@gmail.com>
2020-02-27 21:39:43 +0000

    Done with voting modeulw

--- resources/views/votingindex.blade.php
+++ resources/views/votingindex.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>KT Hall Award Nomination</title>
+    <title>KT Hall Awards Online Voting</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
