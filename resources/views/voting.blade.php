b9c1a3ab51acf38bce0d728a8bebd5a0a9ea11a9
Clement Sam <clementsam75@gmail.com>
2020-02-28 01:55:30 +0000

    Done with voting module

--- resources/views/voting.blade.php
+++ resources/views/voting.blade.php
@@ -67,7 +67,7 @@
                     </div>
                 @endif
                 @if (session()->has('error'))
-                    <div class="alert alert-success">
+                    <div class="alert alert-danger">
                         {{ session()->get('error') ?? '' }}.
                     </div>
                 @endif
