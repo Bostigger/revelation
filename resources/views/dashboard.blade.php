d4b664b793105a70a4ac441efd1fc1901aa1089d
tricksteck <tricksteck@gmail.com>
2020-03-25 19:23:32 +0000

    RE

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -176,22 +176,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-12 col-xl-4">
-                                <div class="card yearly-sales">
-                                    <div class="card-block">
-                                        <h6 class="mb-4">Active Logins</h6>
-                                        <div class="row d-flex align-items-center">
-                                            <div class="col-9">
-                                                <h3 class="f-w-300 d-flex align-items-center  m-b-0"><i class="feather icon-users text-c-green f-30 m-r-10"></i>{{ $page_data['inactiveUsersCount']??0 }}</h3>
-                                            </div>
-
-                                        </div>
-                                        <div class="progress m-t-30" style="height: 7px;">
-                                            <div class="progress-bar progress-c-theme" role="progressbar" style="width: 13%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                          
                             <!--[ year  sales section ] end-->
                             <!--[ Recent Users ] start-->
                             <div class="col-xl-8 col-md-6">
